6365e0431394b64abb21b9da569bfd3bf20ff974
Beljar Hossain <beljarhossain1@gmail.com>
2025-07-14 09:51:44 +0600

    Update start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ngrok http 8000 --authtoken "$NGROK_AUTH_TOKEN" > /dev/null &
+
 sleep 5
+
 NGROK_PUBLIC_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
 
 if [ -z "$NGROK_PUBLIC_URL" ]; then
@@ -10,4 +12,5 @@ if [ -z "$NGROK_PUBLIC_URL" ]; then
 fi
 
 echo "Ngrok Public URL: $NGROK_PUBLIC_URL"
+
 python app.py "$NGROK_PUBLIC_URL"
