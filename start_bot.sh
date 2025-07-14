05edee0f02dc81da50edad54818b3177988be9c1
Beljar Hossain <beljarhossain1@gmail.com>
2025-07-14 09:33:54 +0600

    Add files via upload

---
+++ start_bot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ngrok http 8000 --authtoken "$NGROK_AUTH_TOKEN" > /dev/null &
+sleep 5
+NGROK_PUBLIC_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+if [ -z "$NGROK_PUBLIC_URL" ]; then
+    echo "Failed to get ngrok public URL. Exiting."
+    exit 1
+fi
+
+echo "Ngrok Public URL: $NGROK_PUBLIC_URL"
+python app.py "$NGROK_PUBLIC_URL"
